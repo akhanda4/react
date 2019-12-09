3b8b60db8bc599f8af69fa263c80f7bb6ebcac78
akhanda4 <amanhanda420@gmail.com>
2019-12-09 21:05:55 +0530

    basic comfig folder

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# react
-base config
+run following command to run this project:
+
+1. npm install
+2. npm run build. {if this throw an error of style-loader or css-loader then run command-3 otherwise skip it}
+3. npm install --save-dev style-loader css-loader
+4. npm start
+5. open browser and hit http://localhost:8080/
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Hi! Congratulation you have successfully build this project.</p>
+      </div>
+    );
+  }
+}
